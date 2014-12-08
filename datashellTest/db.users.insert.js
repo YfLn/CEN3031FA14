a4d134d9cc3bdd15a9721bebcdafc2a99a61e66f
kuncao0228 <kuncao0228@ufl.edu>
2014-12-08 16:12:27 -0500

    updated db user insert
    display name, fixed research interest, reorganized format

--- datashellTest/db.users.insert.js
+++ datashellTest/db.users.insert.js
@@ -1,152 +1,243 @@
 db.users.insert(
-{username:'1@ufl.edu',
-password: 'rleon94',
-firstName:'Merrilee',
-lastName:'Melita',
-researchinterests'Literacy and Illiteracy'}
-{username:'2@ufl.edu',
-password: 'rleon94',
-firstName:'Richard',
-lastName:'Huong',
-researchinterests'Forecasting'}
-{username:'3@ufl.edu',
+{
+username:'1@ufl.edu',
 password: 'rleon94',
 firstName:'Tod',
 lastName:'Willene',
-researchinterests'Fairy Tales'}
-{username:'4@ufl.edu',
+displayName:'Tod Willene',
+researchinterests:'Millennium'
+}, 
+{
+username:'2@ufl.edu',
 password: 'rleon94',
-firstName:'Shaquita',
-lastName:'Laurene',
-researchinterests'Labor Unions'}
-{username:'5@ufl.edu',
-password: 'rleon94',
-firstName:'Linwood',
-lastName:'Lora',
-researchinterests'Freedom of Speech'}
-{username:'6@ufl.edu',
-password: 'rleon94',
-firstName:'Altha',
-lastName:'Loma',
-researchinterests'Mental Health'}
-{username:'7@ufl.edu',
-password: 'rleon94',
-firstName:'Shaquita',
-lastName:'Laurene',
-researchinterests'Freedom of Speech'}
-{username:'8@ufl.edu',
-password: 'rleon94',
-firstName:'Kyung',
-lastName:'Sammie',
-researchinterests'Fairy Tales'}
-{username:'9@ufl.edu',
-password: 'rleon94',
-firstName:'Hildred',
-lastName:'Steffanie',
-researchinterests'Millennium'}
-{username:'10@ufl.edu',
+firstName:'Norman',
+lastName:'Cindie',
+displayName:'Norman Cindie',
+researchinterests:'Fashion'
+}, 
+{
+username:'3@ufl.edu',
 password: 'rleon94',
 firstName:'Elayne',
 lastName:'Zenaida',
-researchinterests'Freedom of the Press'}
-{username:'11@ufl.edu',
+displayName:'Elayne Zenaida',
+researchinterests:'Millennium'
+}, 
+{
+username:'4@ufl.edu',
 password: 'rleon94',
-firstName:'Linwood',
-lastName:'Lora',
-researchinterests'Marriage'}
-{username:'12@ufl.edu',
-password: 'rleon94',
-firstName:'Tod',
-lastName:'Willene',
-researchinterests'Malpractice'}
-{username:'13@ufl.edu',
+firstName:'Lauretta',
+lastName:'Whitney',
+displayName:'Lauretta Whitney',
+researchinterests:'Educational Testing'
+}, 
+{
+username:'5@ufl.edu',
 password: 'rleon94',
 firstName:'Shawnna',
 lastName:'Bryanna',
-researchinterests'Endangered Species'}
-{username:'14@ufl.edu',
-password: 'rleon94',
-firstName:'Altha',
-lastName:'Loma',
-researchinterests'Learning Disabilities'}
-{username:'15@ufl.edu',
-password: 'rleon94',
-firstName:'Clement',
-lastName:'Marvel',
-researchinterests'Free and Fair Trade'}
-{username:'16@ufl.edu',
-password: 'rleon94',
-firstName:'Jeanmarie',
-lastName:'Samual',
-researchinterests'Environmental Protection'}
-{username:'17@ufl.edu',
-password: 'rleon94',
-firstName:'Lauretta',
-lastName:'Whitney',
-researchinterests'Endangered Species'}
-{username:'18@ufl.edu',
-password: 'rleon94',
-firstName:'Tod',
-lastName:'Willene',
-researchinterests'Minors Rights'}
-{username:'19@ufl.edu',
-password: 'rleon94',
-firstName:'Kary',
-lastName:'Felisa',
-researchinterests'Junk Food'}
-{username:'20@ufl.edu',
-password: 'rleon94',
-firstName:'Racheal',
-lastName:'Jene',
-researchinterests'Fashion'}
-{username:'21@ufl.edu',
+displayName:'Shawnna Bryanna',
+researchinterests:'Free and Fair Trade'
+}, 
+{
+username:'6@ufl.edu',
 password: 'rleon94',
 firstName:'Linwood',
 lastName:'Lora',
-researchinterests'Freedom of Speech'}
-{username:'22@ufl.edu',
+displayName:'Linwood Lora',
+researchinterests:'Forecasting'
+}, 
+{
+username:'7@ufl.edu',
 password: 'rleon94',
 firstName:'Broderick',
 lastName:'Joana',
-researchinterests'FBI'}
-{username:'23@ufl.edu',
-password: 'rleon94',
-firstName:'Elayne',
-lastName:'Zenaida',
-researchinterests'Juvenile Crime'}
-{username:'24@ufl.edu',
-password: 'rleon94',
-firstName:'Norman',
-lastName:'Cindie',
-researchinterests'Freedom of Speech'}
-{username:'25@ufl.edu',
+displayName:'Broderick Joana',
+researchinterests:'Medical Ethics'
+}, 
+{
+username:'8@ufl.edu',
 password: 'rleon94',
 firstName:'Natacha',
 lastName:'Lazaro',
-researchinterests'Literacy and Illiteracy'}
-{username:'26@ufl.edu',
+displayName:'Natacha Lazaro',
+researchinterests:'Junk Food'
+}, 
+{
+username:'9@ufl.edu',
 password: 'rleon94',
-firstName:'Norman',
-lastName:'Cindie',
-researchinterests'FBI'}
-{username:'27@ufl.edu',
+firstName:'Zena',
+lastName:'Boris',
+displayName:'Zena Boris',
+researchinterests:'Freedom of the Press'
+}, 
+{
+username:'10@ufl.edu',
 password: 'rleon94',
-firstName:'Rafael',
-lastName:'Charleen',
-researchinterests'Malpractice'}
-{username:'28@ufl.edu',
+firstName:'Shawnna',
+lastName:'Bryanna',
+displayName:'Shawnna Bryanna',
+researchinterests:'Junk Food'
+}, 
+{
+username:'11@ufl.edu',
 password: 'rleon94',
-firstName:'Kena',
-lastName:'Emmett',
-researchinterests'Fast Foods'}
-{username:'29@ufl.edu',
+firstName:'Hildred',
+lastName:'Steffanie',
+displayName:'Hildred Steffanie',
+researchinterests:'Learning Disabilities'
+}, 
+{
+username:'12@ufl.edu',
 password: 'rleon94',
-firstName:'Lauretta',
-lastName:'Whitney',
-researchinterests'Freedom of the Press'}
-{username:'30@ufl.edu',
+firstName:'Shaquita',
+lastName:'Laurene',
+displayName:'Shaquita Laurene',
+researchinterests:'Minors Rights'
+}, 
+{
+username:'13@ufl.edu',
+password: 'rleon94',
+firstName:'Shaquita',
+lastName:'Laurene',
+displayName:'Shaquita Laurene',
+researchinterests:'Endangered Species'
+}, 
+{
+username:'14@ufl.edu',
+password: 'rleon94',
+firstName:'Lucinda',
+lastName:'Tayna',
+displayName:'Lucinda Tayna',
+researchinterests:'Environment'
+}, 
+{
+username:'15@ufl.edu',
+password: 'rleon94',
+firstName:'Penni',
+lastName:'Hilary',
+displayName:'Penni Hilary',
+researchinterests:'Labor Unions'
+}, 
+{
+username:'16@ufl.edu',
+password: 'rleon94',
+firstName:'Hildred',
+lastName:'Steffanie',
+displayName:'Hildred Steffanie',
+researchinterests:'Endangered Species'
+}, 
+{
+username:'17@ufl.edu',
+password: 'rleon94',
+firstName:'Broderick',
+lastName:'Joana',
+displayName:'Broderick Joana',
+researchinterests:'Environmental Protection'
+}, 
+{
+username:'18@ufl.edu',
+password: 'rleon94',
+firstName:'Racheal',
+lastName:'Jene',
+displayName:'Racheal Jene',
+researchinterests:'Malpractice'
+}, 
+{
+username:'19@ufl.edu',
+password: 'rleon94',
+firstName:'Broderick',
+lastName:'Joana',
+displayName:'Broderick Joana',
+researchinterests:'Fairy Tales'
+}, 
+{
+username:'20@ufl.edu',
+password: 'rleon94',
+firstName:'Altha',
+lastName:'Loma',
+displayName:'Altha Loma',
+researchinterests:'Environmental Protection'
+}, 
+{
+username:'21@ufl.edu',
+password: 'rleon94',
+firstName:'Daniell',
+lastName:'Marchele',
+displayName:'Daniell Marchele',
+researchinterests:'Literacy and Illiteracy'
+}, 
+{
+username:'22@ufl.edu',
+password: 'rleon94',
+firstName:'Jeanmarie',
+lastName:'Samual',
+displayName:'Jeanmarie Samual',
+researchinterests:'Marriage'
+}, 
+{
+username:'23@ufl.edu',
 password: 'rleon94',
 firstName:'Kary',
 lastName:'Felisa',
-researchinterests'Medical Ethics'}
+displayName:'Kary Felisa',
+researchinterests:'Labor Unions'
+}, 
+{
+username:'24@ufl.edu',
+password: 'rleon94',
+firstName:'Daniell',
+lastName:'Marchele',
+displayName:'Daniell Marchele',
+researchinterests:'Freedom of the Press'
+}, 
+{
+username:'25@ufl.edu',
+password: 'rleon94',
+firstName:'Liana',
+lastName:'Trudy',
+displayName:'Liana Trudy',
+researchinterests:'Elections'
+}, 
+{
+username:'26@ufl.edu',
+password: 'rleon94',
+firstName:'Sherril',
+lastName:'Lisandra',
+displayName:'Sherril Lisandra',
+researchinterests:'Minors Rights'
+}, 
+{
+username:'27@ufl.edu',
+password: 'rleon94',
+firstName:'Racheal',
+lastName:'Jene',
+displayName:'Racheal Jene',
+researchinterests:'Free and Fair Trade'
+}, 
+{
+username:'28@ufl.edu',
+password: 'rleon94',
+firstName:'Natacha',
+lastName:'Lazaro',
+displayName:'Natacha Lazaro',
+researchinterests:'Freedom of Speech'
+}, 
+{
+username:'29@ufl.edu',
+password: 'rleon94',
+firstName:'Natacha',
+lastName:'Lazaro',
+displayName:'Natacha Lazaro',
+researchinterests:'Forecasting'
+}, 
+{
+username:'30@ufl.edu',
+password: 'rleon94',
+firstName:'Jeanmarie',
+lastName:'Samual',
+displayName:'Jeanmarie Samual',
+researchinterests:'Juvenile Crime'
+}, 
 );
+
