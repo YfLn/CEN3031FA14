63d31f37393a44b8b9a7662a0766fb52975d13be
kuncao0228 <kuncao0228@ufl.edu>
2014-12-07 20:30:21 -0500

    datashellTest insert with java Test

---
+++ datashellTest/db.users.insert.js
@@ -0,0 +1,152 @@
+db.users.insert({
+username:'1@ufl.edu',
+password: 'rleon94',
+firstName:'Sherril',
+lastName:'Lisandra',
+researchinterests'Learning Disabilities'
+username:'2@ufl.edu',
+password: 'rleon94',
+firstName:'Daniell',
+lastName:'Marchele',
+researchinterests'Labor Unions'
+username:'3@ufl.edu',
+password: 'rleon94',
+firstName:'Altha',
+lastName:'Loma',
+researchinterests'Learning Disabilities'
+username:'4@ufl.edu',
+password: 'rleon94',
+firstName:'Zena',
+lastName:'Boris',
+researchinterests'Literacy and Illiteracy'
+username:'5@ufl.edu',
+password: 'rleon94',
+firstName:'Racheal',
+lastName:'Jene',
+researchinterests'FBI'
+username:'6@ufl.edu',
+password: 'rleon94',
+firstName:'Richard',
+lastName:'Huong',
+researchinterests'Fashion'
+username:'7@ufl.edu',
+password: 'rleon94',
+firstName:'Jeanmarie',
+lastName:'Samual',
+researchinterests'Literacy and Illiteracy'
+username:'8@ufl.edu',
+password: 'rleon94',
+firstName:'Broderick',
+lastName:'Joana',
+researchinterests'FBI'
+username:'9@ufl.edu',
+password: 'rleon94',
+firstName:'Richard',
+lastName:'Huong',
+researchinterests'Fashion'
+username:'10@ufl.edu',
+password: 'rleon94',
+firstName:'Merrilee',
+lastName:'Melita',
+researchinterests'Marriage'
+username:'11@ufl.edu',
+password: 'rleon94',
+firstName:'Shaquita',
+lastName:'Laurene',
+researchinterests'Freedom of the Press'
+username:'12@ufl.edu',
+password: 'rleon94',
+firstName:'Shawnna',
+lastName:'Bryanna',
+researchinterests'Educational Testing'
+username:'13@ufl.edu',
+password: 'rleon94',
+firstName:'Broderick',
+lastName:'Joana',
+researchinterests'Forecasting'
+username:'14@ufl.edu',
+password: 'rleon94',
+firstName:'Tod',
+lastName:'Willene',
+researchinterests'Freedom of Speech'
+username:'15@ufl.edu',
+password: 'rleon94',
+firstName:'Lauretta',
+lastName:'Whitney',
+researchinterests'Freedom of Speech'
+username:'16@ufl.edu',
+password: 'rleon94',
+firstName:'Tod',
+lastName:'Willene',
+researchinterests'Marriage'
+username:'17@ufl.edu',
+password: 'rleon94',
+firstName:'Broderick',
+lastName:'Joana',
+researchinterests'Emotions'
+username:'18@ufl.edu',
+password: 'rleon94',
+firstName:'Shaquita',
+lastName:'Laurene',
+researchinterests'Environment'
+username:'19@ufl.edu',
+password: 'rleon94',
+firstName:'Merrilee',
+lastName:'Melita',
+researchinterests'Literacy and Illiteracy'
+username:'20@ufl.edu',
+password: 'rleon94',
+firstName:'Merrilee',
+lastName:'Melita',
+researchinterests'Environment'
+username:'21@ufl.edu',
+password: 'rleon94',
+firstName:'Lauretta',
+lastName:'Whitney',
+researchinterests'Labor Unions'
+username:'22@ufl.edu',
+password: 'rleon94',
+firstName:'Chelsea',
+lastName:'Ben',
+researchinterests'Millennium'
+username:'23@ufl.edu',
+password: 'rleon94',
+firstName:'Marcie',
+lastName:'Odilia',
+researchinterests'Freedom of the Press'
+username:'24@ufl.edu',
+password: 'rleon94',
+firstName:'Sherril',
+lastName:'Lisandra',
+researchinterests'Elections'
+username:'25@ufl.edu',
+password: 'rleon94',
+firstName:'Liana',
+lastName:'Trudy',
+researchinterests'Literacy and Illiteracy'
+username:'26@ufl.edu',
+password: 'rleon94',
+firstName:'Tod',
+lastName:'Willene',
+researchinterests'Medical Ethics'
+username:'27@ufl.edu',
+password: 'rleon94',
+firstName:'Linwood',
+lastName:'Lora',
+researchinterests'FBI'
+username:'28@ufl.edu',
+password: 'rleon94',
+firstName:'Tod',
+lastName:'Willene',
+researchinterests'Free and Fair Trade'
+username:'29@ufl.edu',
+password: 'rleon94',
+firstName:'Altha',
+lastName:'Loma',
+researchinterests'Educational Testing'
+username:'30@ufl.edu',
+password: 'rleon94',
+firstName:'Zena',
+lastName:'Boris',
+researchinterests'Marriage'
+});
