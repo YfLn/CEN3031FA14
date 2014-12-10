2a795dafc1943de64d7c026c94168fa37ae44d95
kuncao0228 <kuncao0228@ufl.edu>
2014-12-09 22:41:31 -0500

    database shell insert test

---
+++ datashellTest/db.databases.insert.js
@@ -0,0 +1,182 @@
+db.databases.insert(
+{
+name:'Airiti Inc',
+descrpitonLong:'random description',
+descriptionShort:'random description',
+url:'google.com'
+}, 
+{
+name:'Internet Movie Database',
+descrpitonLong:'random description',
+descriptionShort:'random description',
+url:'google.com'
+}, 
+{
+name:'Allmovie',
+descrpitonLong:'random description',
+descriptionShort:'random description',
+url:'google.com'
+}, 
+{
+name:'ArchINFORM',
+descrpitonLong:'random description',
+descriptionShort:'random description',
+url:'google.com'
+}, 
+{
+name:'All Media Guide',
+descrpitonLong:'random description',
+descriptionShort:'random description',
+url:'google.com'
+}, 
+{
+name:'ArchINFORM',
+descrpitonLong:'random description',
+descriptionShort:'random description',
+url:'google.com'
+}, 
+{
+name:'Philosophy',
+descrpitonLong:'random description',
+descriptionShort:'random description',
+url:'google.com'
+}, 
+{
+name:'Abandoned',
+descrpitonLong:'random description',
+descriptionShort:'random description',
+url:'google.com'
+}, 
+{
+name:'Acronym Finder',
+descrpitonLong:'random description',
+descriptionShort:'random description',
+url:'google.com'
+}, 
+{
+name:'Allmusic',
+descrpitonLong:'random description',
+descriptionShort:'random description',
+url:'google.com'
+}, 
+{
+name:'Airdisaster',
+descrpitonLong:'random description',
+descriptionShort:'random description',
+url:'google.com'
+}, 
+{
+name:'Animal Diversity Web',
+descrpitonLong:'random description',
+descriptionShort:'random description',
+url:'google.com'
+}, 
+{
+name:'Amiga Games Database',
+descrpitonLong:'random description',
+descriptionShort:'random description',
+url:'google.com'
+}, 
+{
+name:'ArchINFORM',
+descrpitonLong:'random description',
+descriptionShort:'random description',
+url:'google.com'
+}, 
+{
+name:'African American Registry',
+descrpitonLong:'random description',
+descriptionShort:'random description',
+url:'google.com'
+}, 
+{
+name:'Arachne',
+descrpitonLong:'random description',
+descriptionShort:'random description',
+url:'google.com'
+}, 
+{
+name:'Allgame',
+descrpitonLong:'random description',
+descriptionShort:'random description',
+url:'google.com'
+}, 
+{
+name:'All Media Guide',
+descrpitonLong:'random description',
+descriptionShort:'random description',
+url:'google.com'
+}, 
+{
+name:'African American Registry',
+descrpitonLong:'random description',
+descriptionShort:'random description',
+url:'google.com'
+}, 
+{
+name:'African American Registry',
+descrpitonLong:'random description',
+descriptionShort:'random description',
+url:'google.com'
+}, 
+{
+name:'Internet Broadway Database',
+descrpitonLong:'random description',
+descriptionShort:'random description',
+url:'google.com'
+}, 
+{
+name:'American National Corpus',
+descrpitonLong:'random description',
+descriptionShort:'random description',
+url:'google.com'
+}, 
+{
+name:'All Media Guide',
+descrpitonLong:'random description',
+descriptionShort:'random description',
+url:'google.com'
+}, 
+{
+name:'Allmusic',
+descrpitonLong:'random description',
+descriptionShort:'random description',
+url:'google.com'
+}, 
+{
+name:'Internet Broadway Database',
+descrpitonLong:'random description',
+descriptionShort:'random description',
+url:'google.com'
+}, 
+{
+name:'Internet Public Library',
+descrpitonLong:'random description',
+descriptionShort:'random description',
+url:'google.com'
+}, 
+{
+name:'Arachne',
+descrpitonLong:'random description',
+descriptionShort:'random description',
+url:'google.com'
+}, 
+{
+name:'Allmovie',
+descrpitonLong:'random description',
+descriptionShort:'random description',
+url:'google.com'
+}, 
+{
+name:'Internet Archive',
+descrpitonLong:'random description',
+descriptionShort:'random description',
+url:'google.com'
+}, 
+{
+name:'Abandoned',
+descrpitonLong:'random description',
+descriptionShort:'random description',
+url:'google.com'
+}, 
+);
